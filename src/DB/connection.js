50ca16b082e807a3bbfff6e253e771771e122c12
Guilherme de Freitas Anacleto <48931728+Gui-Anacleto@users.noreply.github.com>
2022-10-21 21:58:43 -0300

    Arquivo alterado
    Arquivo de connection.js foi alterado:
    adicionado a variável de ambiente.

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose  = require('mongoose')
 
-const uri = "mongodb+srv://root:toor@cluster0.qr1noc5.mongodb.net/?retryWrites=true&w=majority"
+const uri = process.env.STRING_CONNECTION
 
 const connect = async () => {
     db = mongoose.connect(uri)
